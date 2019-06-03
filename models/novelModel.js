daf15f017bf9dc161bb2300e8f1d9e465f84a874
Alaap008 <alaapbanerjee08@gmail.com>
2019-06-03 23:16:06 +0530

    Added Novels Model

---
+++ models/novelModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const novelSchema =new mongoose.Schema({
+    title: { type: String, required: true },
+    idea: { type: String, required: true },
+    genre: { type: String, required: true },
+    status: {type:String, required: true},
+    content: {type:String, required:true},
+    mainauthor: {type: mongoose.Schema.Types.ObjectId,
+        required: true, ref: 'Authors'},
+    collabauthor: [{type: mongoose.Schema.Types.ObjectId,
+        required: true, ref: 'Authors'}]
+});
+
+module.exports = mongoose.model('Novels', novelSchema);
