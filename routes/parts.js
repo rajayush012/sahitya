1f0a10ba76fed58e03b97867656c789038024310
Alaap008 <alaapbanerjee08@gmail.com>
2019-06-04 16:36:26 +0530

    Any change

--- routes/parts.js
+++ routes/parts.js
@@ -2,10 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const router = express.Router();
-const Parts= require ('../models/partModel');
-const Novels = require('../models/novelModel');
+const parts= require ('../models/partModel');
 
-router.get('/',(req,res)=>{
+
+router.get('/',isLoggedIn,(req,res)=>{
     //console.log(req.user)
     // novels.find({}, (err, menuItems)=>{
     //     if (err){
@@ -19,31 +19,20 @@ router.get('/',(req,res)=>{
     // })
     res.send('Onparts')
 })
-
-router.get('/:novelid',(req,res)=>{
-    Parts.find({novelid:req.params.novelid}, (err,parts)=>{
-        if(err){
-            console.log(err);
-        }else{
-            res.render('parts/allparts',{parts:parts});
-        }
-    });
-})
-
-router.get('/:novelid/addpart',(req,res)=>{
-   res.render('parts/addPart');
-});
-
-router.post('/:novelid/addpart',(req, res)=>{
- //   console.log(req.user);
-    Parts.create({
-        title: req.body.tile,
-        novelid: req.params.novelid,
-        collabauthor: req.user._id,
-        partcontent: req.body.partcontent
-    });
-    res.send('Success');
+router.post('/',(req, res)=>{
+ parts.create({
+     
+ })
    
 })
+
+function isLoggedIn(req,res,next){
+    // console.log(req.isAuthenticated());
+     if(req.isAuthenticated()){
+         return next();
+     }
+     res.redirect('/author/login');
+ }
+ 
 
 module.exports = router;
