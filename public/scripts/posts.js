31ef699585f0a50cfabdbcf134107884b8f255a5
Ayush Raj <raj.ayush012@gmail.com>
2019-06-06 02:01:34 +0530

    small changes

--- public/scripts/posts.js
+++ public/scripts/posts.js
@@ -1,10 +1,47 @@
+var novs = [];
+
 axios.get('http://localhost:3000/novels/allnovels')
 .then((respose)=>{
     console.log(respose);
-
-
+    respose.data.novs.forEach((item)=>{
+        novs.push(item);
+        document.getElementById('lidisp').innerHTML+=`<a class="m-5" href="/novels/${item._id}" style="text-decoration:none">
+        <div class="card">
+          <div class="card-body">
+            <h2 class="car-title">${item.title}</h2>
+            <p class="card-subtitle text-muted">by - ${item.mainauthor.name}</p> <!--add XHR Req here for username-->
+            <p class="text-muted">Idea - ${item.idea}</p>
+          </div>
+        </div>
+      </a>`
+    });
     
+
 })
 .catch((err)=>{
     console.log(err);
 })
+
+
+function filterNovels(element){
+    var x = element.value;
+    
+    document.getElementById('lidisp').innerHTML = "";
+    novs.forEach((item)=>{
+        if(item.genre === x){
+        document.getElementById('lidisp').innerHTML+=`<a class="m-5" href="/novels/${item._id}" style="text-decoration:none">
+        <div class="card">
+          <div class="card-body">
+            <h2 class="car-title">${item.title}</h2>
+            <p class="card-subtitle text-muted">by - ${item.mainauthor.name}</p> <!--add XHR Req here for username-->
+            <p class="text-muted">Idea - ${item.idea}</p>
+          </div>
+        </div>
+      </a>`
+        }
+
+        
+    });
+
+}
+
