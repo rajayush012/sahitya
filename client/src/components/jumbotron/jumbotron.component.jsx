cd31dadb2ffa05a477ab777ac2057da46f281d29
Ayush Raj <raj.ayush012@gmail.com>
2020-10-04 23:38:31 +0530

    Added hero banner to homepage

--- client/src/components/jumbotron/jumbotron.component.jsx
+++ client/src/components/jumbotron/jumbotron.component.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './jumbotron.styles.scss'
 import Signup from '../signup/signup.component';
+import url from '../../assets/hero.png'
 
 export default function Jumbotron() {
     return (
         <div className='jumbotron'>
-            <div className='hero'>
-            <h1>Sahitya.</h1>
+            <div className='hero'
+                style={{
+                    backgroundImage: `url(${url})`,
+                    backgroundSize : 'cover',
+                    backgroundPosition: '50% 0'
+                    
+                }}
+            >
+            
             </div>
             <Signup/>
         </div>
