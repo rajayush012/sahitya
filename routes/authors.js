ea415da95a6c44b603879555352a2508e95687b6
Alaap008 <alaapbanerjee08@gmail.com>
2020-10-04 23:52:51 +0530

    Added jwt authentication

--- routes/authors.js
+++ routes/authors.js
@@ -189,72 +189,70 @@ router.post("/signup", (req, res, next) => {
 //       user.password = hash;
 //       next();
 //     })
-//   });
-
-router.get('/dashboard',isLoggedIn,(req,res)=>{
-    //console.log(req.user);
-    var novparts = new Array();
-    var ob = {};
-   Novels.find({'mainauthor.id': req.user._id},(err,novels)=>{
-        if(err){
-            console.log(err);
-        }else{
+// router.get('/dashboard',isLoggedIn,(req,res)=>{
+//     //console.log(req.user);
+//     var novparts = new Array();
+//     var ob = {};
+//    Novels.find({'mainauthor.id': req.user._id},(err,novels)=>{
+//         if(err){
+//             console.log(err);
+//         }else{
            
-            Parts.find({'collabauthor.id' : req.user._id }, (err,parts)=>{
-                if(err){
-                    console.log(err);
-                }else{
+//             Parts.find({'collabauthor.id' : req.user._id }, (err,parts)=>{
+//                 if(err){
+//                     console.log(err);
+//                 }else{
                     
-                    res.render('author/dashboard',{novels:novels, parts:parts }); 
-                }});
-        }
-   });
+//                     res.render('author/dashboard',{novels:novels, parts:parts }); 
+//                 }});
+//         }
+//    });
     
-});
+// });
 
-router.get('/signup',(req,res)=>{
-    res.render('author/signup');
-});
+// router.get('/signup',(req,res)=>{
+//     res.render('author/signup');
+// });
 
-router.post('/signup',(req,res)=>{
-    var newAuthor = new Author({username: req.body.username, email: req.body.email,name: req.body.name});
-    Author.register(newAuthor,req.body.password, (err,author)=>{
-        if(err){
-            console.log(err);
-            res.redirect('/')
-        }
-        passport.authenticate("local")(req,res, ()=>{
-        res.redirect('/novels')
-        })
-    } );
-});
+// router.post('/signup',(req,res)=>{
+//     var newAuthor = new Author({username: req.body.username, email: req.body.email,name: req.body.name});
+//     Author.register(newAuthor,req.body.password, (err,author)=>{
+//         if(err){
+//             console.log(err);
+//             res.redirect('/')
+//         }
+//         passport.authenticate("local")(req,res, ()=>{
+//         res.redirect('/novels')
+//         })
+//     } );
+// });
 
-router.get('/login',(req,res)=>{
-    res.render('author/login');
-});
+// router.get('/login',(req,res)=>{
+//     res.render('author/login');
+// });
 
-router.post('/login',passport.authenticate("local",{
-    successRedirect: "/novels",
-    failureRedirect: "/author/login"
-}),(req,res)=>{
+// router.post('/login',passport.authenticate("local",{
+//     successRedirect: "/novels",
+//     failureRedirect: "/author/login"
+// }),(req,res)=>{
     
-});
+// });
 
-router.get('/logout',(req,res)=>{
-    req.logOut();
-    res.redirect('/');
-})
+// router.get('/logout',(req,res)=>{
+//     req.logOut();
+//     res.redirect('/');
+// })
 
 
-router.get('/dahboard',(req,res)=>{
-    res.render('author/dashboard');
-})
-function isLoggedIn(req,res,next){
-   // console.log(req.isAuthenticated());
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect('/author/login');
-}
+// router.get('/dahboard',(req,res)=>{
+//     res.render('author/dashboard');
+// })
+// function isLoggedIn(req,res,next){
+//    // console.log(req.isAuthenticated());
+//     if(req.isAuthenticated()){
+//         return next();
+//     }
+//     res.redirect('/author/login');
+// }
 
 module.exports = router;
