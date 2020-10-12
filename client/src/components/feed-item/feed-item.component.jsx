885bcda60282c37cdef0b3d0f37d6af6a65bc43e
Ayush Raj <raj.ayush012@gmail.com>
2020-10-12 22:04:59 +0530

    Feed details now visibile

--- client/src/components/feed-item/feed-item.component.jsx
+++ client/src/components/feed-item/feed-item.component.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import './feed-item.styles.scss'
 
 export default function FeedItem(props) {
     const {feed} = props
     return (
         <div className='feed-item'>
-                
-                    <h2> {feed.title}</h2>
+            <div>{feed.title}</div>
+            <div className='sub'>
+                <div className='author'>{feed.mainauthor.name}</div>
+                <div className='genre'>Genre - {feed.genre}</div>
+            </div>
+            <div className='idea'>Idea - {feed.idea}</div>
+            
         </div>
     )
 }
